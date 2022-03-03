1c3413eac9c22ac2bcbe0e22bef3c14788ab15cd
Jason Shaw <clients@uptocodetech.com>
2022-03-03 15:25:51 -0500

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const https = require('https');
 const path = require('path');
+const dns = require('dns');
 
 const app = express();
 const port = 3000;
@@ -16,24 +17,47 @@ app.use(bodyParser.json());
 // allow folder route for all public static assets
 app.use(express.static('public'));
 
+// load helper functions
+const servers = require("./utils/fetchServerList.js");
+
 // GET home endpoint
 app.get('/', (req, res) => {
 	//todo send a dynamic security code as well
     res.sendFile(path.join(__dirname, '/public/dnslookup.html'));
 });
 
-// GET DNS results endpoint
+// GET DNS results endpoint...
 let dnsResults = [];
 
 app.get('/dns', (req, res) => {
 	let domain = req.query.domain;
 	let type = req.query.type;
-	https.get('https://dns.google/resolve?name='+domain+'&type='+type, resp => {
+	let server = servers.getRandomServer();
+
+	console.log('fetching from '+server);
+
+	//set target dns server and run the query
+	dns.setServers( [server] );
+	dns.resolve(domain, type, function (error, addresses) {
+		console.log("Error: ", error);  // null
+		console.log("DNS servers: ", dns.getServers());  // [ '8.8.8.8', '8.8.4.4' ]
+		console.log(domain + " resolves to: ", addresses);  // [ '192.168.0.10' ]
+	});
+
+	//this is google's implementation
+	/* https.get(server+'?name='+domain+'&type='+type, resp => {
         resp.on('data', (d) => {
 			dnsResults = JSON.parse(d);
-			res.json(dnsResults['Answer']);
+			if (type !== 'MX') {
+				res.json(dnsResults['Answer']);
+			} else {
+				res.json(dnsResults['Authority']);
+			}
+
     	});
 	});
+
+	 */
 });
 
 // front end routes
---
+++ utils/fetchServerList.js
@@ -0,0 +1,24 @@
+let serverList = [
+	'162.243.19.47', //this
+	'9.9.9.9', //that
+	'149.112.112.112',
+	'8.8.8.8'
+];
+
+/*
+
+let serverList = [
+	['USA', 'Mountain View, California', 'Google', '8.8.8.8'],
+	['USA', 'Berkeley, California', 'Quad9', '9.9.9.9'],
+];
+
+
+
+ */
+
+
+function getRandomServer() {
+	return serverList[serverList.length * Math.random() | 0];
+}
+
+module.exports = { getRandomServer };
